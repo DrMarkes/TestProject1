984bbb0ec6a60c91b801ab374c071c70aaf6508d
Roman Markov <markovroma@gmail.com>
2021-12-21 17:57:30 +0700

    deploy

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+// Полифилл для IE11 foreach
+if ('NodeList' in window && !NodeList.prototype.forEach) {
+    console.info('polyfill for IE11');
+    NodeList.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (var i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+}
 
 // Поведение фильтра на мобильных устройствах
 document.querySelector('.menu-icon-wrapper').onclick = () => {
@@ -23,8 +33,49 @@ const widgets = document.querySelectorAll('.widget');
 widgets.forEach((widget) => {
     const widgetTitle = widget.querySelector('.widget__title');
     const widgetBody = widget.querySelector('.widget__body');
-    widgetTitle.addEventListener('click', () => {
-        widgetTitle.classList.toggle('widget__title--toggle');
-        widgetBody.classList.toggle('widget__body--hidden');
+    if(widgetTitle) {
+        widgetTitle.addEventListener('click', () => {
+            widgetTitle.classList.toggle('widget__title--toggle');
+            widgetBody.classList.toggle('widget__body--hidden');
+        });
+    }
+});
+
+// Выбор параметров в виджете "Расстояние до метро"
+const distanceCheckboxAll = document.querySelectorAll('.distance__checkbox');
+const anyDistanceCheckbox = document.querySelector('#distance-05');
+
+distanceCheckboxAll.forEach((distanceCheckbox) => {
+    distanceCheckbox.addEventListener('change', () => {
+        if(distanceCheckbox === anyDistanceCheckbox) {
+            distanceCheckboxAll.forEach((item) => {
+                item.checked = false;
+            });
+            distanceCheckbox.checked = true;
+        } else {
+            if(anyDistanceCheckbox.checked) {
+                anyDistanceCheckbox.checked = false;
+            }
+        }
     });
 });
+
+// Показать/Скрыть дополнительные опции при клике на кнопку в в виджете Дополнительные опции
+const btnShowOptions = document.querySelector('.widget__btn-show-hidden');
+
+const hiddenOptions = document.querySelectorAll('[data-option-hidden=hidden]');
+
+btnShowOptions.addEventListener('click', () => {
+    hiddenOptions.forEach((item) => {
+        if(item.getAttribute('data-option-hidden') === 'hidden') {
+            item.style.display= 'flex';
+            btnShowOptions.textContent = 'Скрыть доп. опции';
+            item.setAttribute('data-option-hidden', 'visible');
+        } else {
+            item.style.display = 'none';
+            item.setAttribute('data-option-hidden', 'hidden');
+            btnShowOptions.textContent = 'Показать ещё';
+        }
+    });
+});
+
